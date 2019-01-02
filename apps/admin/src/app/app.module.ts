ed677ff9a2bca1ebc4b7dfcb6ec28082d7f3fa83
harbirchahal <harbirchahal@hotmail.com>
2019-01-03 00:54:13 +0530

    update admin app

--- apps/admin/src/app/app.module.ts
+++ apps/admin/src/app/app.module.ts
@@ -1,19 +1,50 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { RouterModule } from '@angular/router';
-import { MyOrgHeaderModule } from '@myorg/ui/header';
+import { HttpClientModule } from '@angular/common/http';
 import { NxModule } from '@nrwl/nx';
+import { EffectsModule } from '@ngrx/effects';
+import { StoreModule } from '@ngrx/store';
+import { StoreDevtoolsModule } from '@ngrx/store-devtools';
+import { StoreRouterConnectingModule } from '@ngrx/router-store';
+import { storeFreeze } from 'ngrx-store-freeze';
+import { MaterialModule } from '@myorg/ui/libs';
+import { MyOrgAuthModule } from '@myorg/auth';
+import { MyOrgProfileModule } from '@myorg/profile';
+import { MyOrgHeaderModule } from '@myorg/ui/header';
+import { MyOrgProgressModule } from '@myorg/ui/progress';
+import { MyOrgMessagesModule } from '@myorg/ui/messages';
+import { environment } from '../environments/environment';
+import { AppRouterModule } from './app.routes';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
 
 @NgModule({
-  declarations: [AppComponent],
   imports: [
     BrowserModule,
     BrowserAnimationsModule,
+    HttpClientModule,
+    MyOrgAuthModule,
+    MyOrgProfileModule,
     MyOrgHeaderModule,
+    MyOrgProgressModule,
+    MyOrgMessagesModule,
+    MaterialModule,
+    AppRouterModule,
     NxModule.forRoot(),
-    RouterModule.forRoot([], { initialNavigation: 'enabled' })
+    StoreModule.forRoot(
+      {},
+      { metaReducers: !environment.production ? [storeFreeze] : [] }
+    ),
+    EffectsModule.forRoot([]),
+    StoreRouterConnectingModule,
+    !environment.production ? StoreDevtoolsModule.instrument() : []
+  ],
+  declarations: [
+    AppComponent,
+    HomeComponent,
+    DashboardComponent
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ apps/admin/src/app/app.routes.ts
@@ -0,0 +1,39 @@
+import { NgModule } from '@angular/core';
+import { Route, RouterModule } from '@angular/router';
+import { AUTH_ROUTES, AuthGuard } from '@myorg/auth';
+import { PROFILE_ROUTES } from '@myorg/profile';
+import { HomeComponent } from './home/home.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
+
+export const routes: Route[] = [
+  {
+    path: '',
+    redirectTo: 'app',
+    pathMatch: 'full'
+  },
+  ...AUTH_ROUTES,
+  {
+    path: 'app',
+    component: HomeComponent,
+    canActivate: [AuthGuard],
+    children: [
+      {
+        path: '',
+        component: DashboardComponent,
+        pathMatch: 'full'
+      },
+      ...PROFILE_ROUTES
+    ]
+  },
+];
+
+@NgModule({
+  imports: [
+    RouterModule.forRoot(routes, {
+      initialNavigation: 'enabled',
+      enableTracing: false
+    })
+  ],
+  exports: [RouterModule]
+})
+export class AppRouterModule { }
